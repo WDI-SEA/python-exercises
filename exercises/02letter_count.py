01644beed2b9983ec4a43fdaaa0211537327e786
Steve Geluso <stevegeluso@gmail.com>
2017-01-09 10:46:44 -0800

    rewriting exercises

---
+++ exercises/02letter_count.py
@@ -0,0 +1,8 @@
+# Write a method called `letter_count` to count the letter
+# occurrence in a string. Use a dictionary.
+#
+# Example method call:
+#
+# letter_count('banana')
+#
+# > {'a': 3, 'b': 2}
---
+++ exercises/04multiply_by.py
@@ -0,0 +1,13 @@
+# Write a method called `multiply_by` that takes a number and
+# array, and returns the array of numbers multiplied by that number.
+# (Hint, use the .map function)
+#
+# map(lambda n: n * 3, [12,3,4])
+#
+# Example method call:
+#
+# multiply_by([1, 2, 3], 5)
+#
+# > 5
+# > 10
+# > 15
