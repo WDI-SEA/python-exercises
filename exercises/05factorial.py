c5b26da8a01c3b7b7f1b149d4fab0f5cd6b4777c
idothestamping <douglas.klemp@gmail.com>
2019-02-14 10:50:57 -0800

    homework fixed #5

--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -13,6 +13,6 @@ def factorial(n):
     if n == 0:
         return 1
     else:
-      return n * factorial(n-1)
-n=int(input("Input a number to compute the factiorial : " + factorial(n)))
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
 print(factorial(n))
