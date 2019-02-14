f3f91c93bf04b6b1484b9b59ee3b6d05ba77a843
Nhuuu <nqtrinh87@gmail.com>
2019-02-14 09:46:44 -0800

    finished deliverable

--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -1,11 +1,19 @@
-# Write a method to compute the `factorial` of a number.
-# Given a whole number n, a factorial is the product of all
-# whole numbers from 1 to n.
-# 5! = 5 * 4 * 3 * 2 * 1
-#
-# Example method call
-#
-# factorial(5)
-#
-# > 120
-#
+`  # Write a method to compute the `factorial` of a number.
+  # Given a whole number n, a factorial is the product of all
+  # whole numbers from 1 to n.
+  # 5! = 5 * 4 * 3 * 2 * 1
+  #
+  # Example method call
+  #
+  # factorial(5)
+  #
+  # > 120
+  #`
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Input a number to compute the factiorial : "))
+print(factorial(n))
