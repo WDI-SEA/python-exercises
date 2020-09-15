d80e4ab23dd75f197060fa271ac64cecc337d016
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-09-14 18:26:54 -0700

    answer to 01loop.py

--- exercises/01loop.py
+++ exercises/01loop.py
@@ -1,14 +1,20 @@
 # Write a method called `p_times` that takes a `statement` and
 # a `num` as inputs, and outputs the `statement` some `num` of times
 # to the console.
+
+def p_times(statement, num):
+  range
+  for i in range(num):
+    print(statement)
+
 #
 # Example method call:
 #
-# p_times('Hello there', 1)
+p_times('Hello there', 1)
 #
 # > Hello there
 #
-# p_times('Hello there', 3)
+p_times('Hello there', 3)
 #
 # > Hello there
 # > Hello there
