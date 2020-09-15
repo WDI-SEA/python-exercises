9b6e8b4dc5223d09325c2f7da12c05ec24225039
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-09-14 20:34:00 -0700

    05factorial answer

--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -3,9 +3,20 @@
 # whole numbers from 1 to n.
 # 5! = 5 * 4 * 3 * 2 * 1
 #
+
+def factorial(n):
+  result = 1
+  for i in range(1, n + 1):
+    print(f"this is printing i - {i}")
+    result = result * int(i)
+    print(f"this is printing result - {result}")
+
+
+
+
 # Example method call
 #
-# factorial(5)
+factorial(5)
 #
 # > 120
 #
