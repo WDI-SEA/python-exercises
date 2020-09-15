eb2c5a58ab945c415ff664696be8ea333012447e
blangwell <b.langwell@outlook.com>
2020-09-14 18:00:51 -0700

    [factorial] Add factorial solution method

--- exercises/05factorial.py
+++ exercises/05factorial.py
@@ -9,3 +9,18 @@
 #
 # > 120
 #
+
+def factorial(num):
+  result = 0
+  # invoke reversed() to reverse the range (fun and new!)
+  for i in reversed(range(1, num + 1)):
+    # account for fencepost of first num and add it directly to result
+    if (i == num):
+      result += num
+    # if it is not the first number, multiply the result by i
+    else:
+      result *= i
+  print(result)
+
+
+factorial(10)
