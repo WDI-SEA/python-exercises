baa778fbe904843e83efc454a9f9a7a8785a5d0e
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-09-14 18:45:27 -0700

    03print_contacts answer

--- exercises/03print_contacts.py
+++ exercises/03print_contacts.py
@@ -20,3 +20,9 @@ contacts = {
   'Lenny': '444-444-4444',
   'Daniel': '777-777-7777'
 }
+
+def print_contacts(cnt):
+  for key, value in cnt.items():
+    print(f"{key} has a phone number of {value}")
+
+print_contacts(contacts)
